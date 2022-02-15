b2e93455521d0ad39f7baa192f78de02c14cacb1
김용건 (Kim Younggeon) <mail@kyg.kr>
2022-02-15 16:17:17 +0900

    feat: add cors

--- apps/api/src/main/kotlin/yourpackage/api/global/security/WebSecurityConfig.kt
+++ apps/api/src/main/kotlin/yourpackage/api/global/security/WebSecurityConfig.kt
@@ -2,8 +2,9 @@
 package yourpackage.api.global.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.POST
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
 import yourpackage.api.global.error.Errors
 
 @Configuration
@@ -18,7 +20,10 @@ import yourpackage.api.global.error.Errors
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 class WebSecurityConfig(
   private val jwtAuthFilter: JwtAuthFilter,
-  private val om: ObjectMapper
+  private val om: ObjectMapper,
+
+  @Value("\${api.web.base}")
+  private val webBase: String,
 ) : WebSecurityConfigurerAdapter() {
   override fun configure(http: HttpSecurity) {
     // JWT 인증을 사용하므로 필요없는 항목 제거
@@ -40,9 +45,15 @@ class WebSecurityConfig(
     // JWT 필터 추가
     http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 
+    // CORS 설정
+    http.cors().configurationSource { CorsConfiguration().apply {
+      allowedOrigins = listOf(webBase)
+      allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE")
+    }}
+
     // 인증 경로
     http.authorizeRequests()
-      .mvcMatchers(HttpMethod.POST, "/auth").anonymous() // 로그인
+      .mvcMatchers(POST, "/auth").anonymous() // 로그인
       .anyRequest().fullyAuthenticated() // 기본 값은 전체 인증필요
   }
 }
