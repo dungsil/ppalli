96d403e7140030683b73652e678d705dd1602224
Kim Younggeon <mail@kyg.kr>
2022-07-12 19:27:33 +0900

    refactor!: 인증 API 경로 변경

--- api/main/kotlin/com/yourcompany/auth/AuthController.kt
+++ api/main/kotlin/com/yourcompany/auth/AuthController.kt
@@ -2,18 +2,22 @@ package com.yourcompany.auth
 
 import com.yourcompany.account.Account
 import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.security.access.annotation.Secured
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
-import java.util.*
+import java.util.UUID
 import javax.validation.Valid
 
 @RestController
+@RequestMapping("/auth/token")
 class AuthController(private val srv: AuthService) {
 
   /**
    * 회원 인증 ( 로그인 )
    */
-  @PostMapping("/authorize")
+  @PostMapping
+  @PreAuthorize("hasRole('ROLE_ANONYMOUS')")
   fun authorize(@Valid @RequestBody req: AuthRequest): AuthResponse {
     val account = srv.login(req.username, req.password)
     return srv.issueToken(account)
@@ -22,7 +26,8 @@ class AuthController(private val srv: AuthService) {
   /**
    * 토큰 재발급
    */
-  @PostMapping("/authorize/refresh")
+  @PostMapping("/refresh")
+  @Secured("permitAll")
   fun refresh(@CookieValue("refresh_token") refreshToken: UUID): AuthResponse {
     return srv.refreshToken(refreshToken)
   }
@@ -31,7 +36,7 @@ class AuthController(private val srv: AuthService) {
    * 토큰 취소
    */
   @ResponseStatus(NO_CONTENT)
-  @DeleteMapping("/authorize")
+  @DeleteMapping
   fun revoke(authentication: Authentication) {
     val account = authentication.principal as Account
     return srv.revokeToken(account)
